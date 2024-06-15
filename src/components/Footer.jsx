2e8ad43a8cb8ed6c420f4fd00b5512c6d9931842
Priyanshu Gupta <priyanshu16095@gmail.com>
2024-06-15 12:13:24 +0530

    some changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,8 +10,8 @@ function Footer() {
                     <p className="font">CAR Rental</p>
                     <p className='info'>We offers a big range of vehicles for all your driving needs. We have the perfect car to meet your needs.</p>
                     <div className='flex-v gap-xs'>
-                        <p>+91-9027644034</p>
-                        <p>priyanshu16095@gmail.com</p>
+                        <p className='link'>+91-9027644034</p>
+                        <p className='link'>priyanshu16095@gmail.com</p>
                     </div>
                 </div>
 
@@ -19,20 +19,20 @@ function Footer() {
                     <div className="footer__box flex-v gap">
                         <p className="font">Company</p>
                         <div className="flex-v gap-xs">
-                            <p>New York</p>
-                            <p>Carrers</p>
-                            <p>Mobile</p>
-                            <p>Blog</p>
-                            <p>How we work</p>
+                            <p className='link'>New York</p>
+                            <p className='link'>Carrers</p>
+                            <p className='link'>Mobile</p>
+                            <p className='link'>Blog</p>
+                            <p className='link'>How we work</p>
                         </div>
                     </div>
 
                     <div className="footer__box flex-v gap">
                         <p className="font">Working Hours</p>
                         <div className="flex-v gap-xs">
-                            <p>Mon - Fri: 9:00AM - 9:00PM</p>
-                            <p>Sat: 9:00AM - 19:00PM</p>
-                            <p>Sun: Closed</p>
+                            <p className='link'>Mon - Fri: 9:00AM - 9:00PM</p>
+                            <p className='link'>Sat: 9:00AM - 19:00PM</p>
+                            <p className='link'>Sun: Closed</p>
                         </div>
                     </div>
                 </div>
